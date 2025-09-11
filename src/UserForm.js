97c6635da24882bcaaf35a6443b1cad458b6a5ad
DulangaNikeshala <165166019+Dulanga2002@users.noreply.github.com>
2025-09-12 00:07:35 +0530

    created the get users feature

--- src/UserForm.js
+++ src/UserForm.js
@@ -78,7 +78,7 @@ const UserForm = (props) => {
 
                     }
         }}
-      >Submit</Button>
+      >Add</Button>
     </Grid>
   );
 };
--- src/Users.js
+++ src/Users.js
@@ -1,14 +1,27 @@
 import { Box } from "@mui/material";
 import UserForm from "./UserForm";
 import UsersTable from "./UsersTable";
-
-const users = [
-    { id: 1, name: "Dulanga" },
-    { id: 2, name: "Dasun" }, 
-];
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect( () => {
+        getUsers();
+    }, []);
+
+    const getUsers = () => {
+        axios.get('http://localhost:3001/api/users')
+        .then( response => {
+           setUsers(response?.data?.response);
+        })
+        .catch( error => {
+            console.log("axios error",error);
+        });
+
+    }
 return (
     <Box 
         sx={{
