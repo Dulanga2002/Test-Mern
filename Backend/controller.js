bf7c16d9556c70fdb774cf71be891802a17af653
DulangaNikeshala <165166019+Dulanga2002@users.noreply.github.com>
2025-09-11 16:52:47 +0530

    Add MongoDB integration and backend CRUD operations

--- Backend/controller.js
+++ Backend/controller.js
@@ -1,22 +1,27 @@
-const users = [
-    {
-        id: 1,
-        name: "Dulanga"
-    },
-    {
-        id: 2,
-        name: "Dasun"
-    }
+const { response } = require('./app');
+const User = require('./model');
 
-];
-const getUsers = (cb) => {
-    cb(users);
+
+const getUsers = (req,res,next) => {
+    User.find()
+    .then(response => {
+        res.json({
+            response
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    });
 };
 
-const getUserById = (id, cb) => {
-    const user = users.find(user => user.id === parseInt(id));
-    cb(user);
-};
+const addUsers = (req,res,next) => {
+    const user = new User({ 
+        id: req.body.id,
+        name: req.body.name
+
+}
 
 exports.getUsers = getUsers;
 exports.getUserById = getUserById;
