4f4a437c50440b240d1d70f9922cdbf7713c0e24
DulangaNikeshala <165166019+Dulanga2002@users.noreply.github.com>
2025-09-12 03:44:09 +0530

    fix server errors

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ const connect = async() => {
 connect();
 
 const server = app.listen(3001,'127.0.0.1', () => {
-    console.log(`Node server is listening to ${server.address().port}`);
+    console.log(`Node server is listening to port ${port}`);
 });
 
 app.use('/api', router);
